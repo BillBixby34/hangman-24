69cc888f4963971db5ae446908829c792675ce9f
Will H <w.j.hilliard28@gmail.com>
2024-04-21 23:29:10 -0400

    If user selection is not a match, game breaks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,9 +7,9 @@ let userGuess = "";
 //will store user selections
 let userGuessArray = [];
 //randomWord will be the word chosen from listOfPals
-let randomWord = [];
+let randomWord = "";
 //randomLetter will be the randomWord seperated into an array by letter
-let randomLetter;
+let randomLetter = [];
 //blankArray will have dashes until letters revealed
 let blankArray =[];
 //blanks will hold the number of blanks per randomLetter length
@@ -20,6 +20,23 @@ let blankArrayText = document.getElementById("blank-space");
 let guesses = 7;
 let wins = 0;
 let losses = 0;
+
+
+//update score function
+function updateScore(){
+  document.getElementById("wins").innerHTML = wins;
+  document.getElementById("losses").innerHTML = losses;
+  document.getElementById("guesses").innerHTML = guesses;
+  document.getElementById("user-guesses").innerHTML = userGuessArray;
+ }
+
+ //function to set image to div
+function setImage(image){
+  let img = document.createElement("IMG");
+      img.src = "images/"+image;
+      document.getElementById('image').appendChild(img);
+  }
+
 //game start function
 function gameStart(){
   guesses = 7;
@@ -28,102 +45,85 @@ function gameStart(){
   //split randomWord into an array
   randomLetter = randomWord.split("");
   //set blanks equal to length of randomLetter array
-  // blanks = randomLetter.length;
-  for (let i = 0; i < randomWord.length; i++) {
+  blanks = randomLetter.length;
+  //reset blankArray and userGuesses array
+  blankArray = [];
+  userGuessArray = [];
+  for (let i = 0; i < blanks; i++) {
     blankArray.push(" _ ");
-    console.log(blankArray);
-
   }
+  console.log("The blankArray looks like " + blankArray)
   //set blankArray to html
-  blankArrayText.textContent = blankArray.join(" ");
+  blankArrayText.innerHTML = blankArray.join(" ");
+  //clear user-guesses array
+  document.getElementById("user-guesses").innerHTML = userGuessArray.join(" ");
+  document.getElementById("guesses").innerHTML = guesses;
 }
 
-//update score function
-function updateScore(){
-  document.getElementById("wins").innerHTML( wins);
-  document.getElementById("losses").innerHTML(losses);
-  document.getElementById("guesses").innerHTML(guesses);
- }
 
-//function to set image to div
-function setImage(image){
-let img = document.createElement("IMG");
-    img.src = "images/"+image;
-    document.getElementById('image').appendChild(img);
-}
+
+
 //test the gameStart function
-gameStart();
-console.log("The blankArray looks like " + blankArray)
+//gameStart();
+
 //choose pal at index of array??
 
-//check if game is over
-function scoreGame(){
-  document.getElementById("wins").innerHTML = wins;
-  document.getElementById("losses").innerHTML = losses;
-  document.getElementById("guesses").innerHTML = guesses;
-  if (guesses = 0) {
-    alert ("game over");
-    losses++;
-    //update html to show loss
-    //updateScore();
-    gameStart();
-    //if player has guessed the word
-  } else if (blankArray === randomLetter){
-    alert("You win!");
-    wins++;
-    setImage(randomWord);
-    //update html to show win
-   // updateScore();
-    gameStart();
+//gameplay function
+function gamePlay(letter){
 
+  //this will update if the letter is there
+
+  let chosenLetter = false;
+  for (let i = 0; i < blanks; i++) {
+    //CHECK IF WORKS
+    if (randomLetter[i] === userGuess) {
+      chosenLetter = true;
+    };
   }
-};
+      if(chosenLetter){
+        //loop will go through array to find each instance of the letter
+        for (let j = 0; j < blanks; j++) {
+          blankArray[j] = userGuess;
+          
+        }
+      }
+    
+    else{
+      userGuessArray.push(userGuess);
+      guesses--;
+
+    }
+  };
+    
+  
+
    //update score function
-   
+   function scoreGame(){
+    updateScore();
+    if (blankArray === randomWord) {
+      wins++;
+      alert("You win");
+      setImage(randomWord);
+      console.log("The image should be " + randomWord);
+      gameStart();
+
+    } else if(guesses = 0){
+      losses++;
+      alert("game over");
+      gameStart();
+    }
+   }
+  
+   gameStart();
 
 document.onkeyup = function(event) {
 //want to add space bar to start game***
     //read user iputs
-    let userGuess = event.key;
-    // Alerts the key the user pressed (userGuess).
-    alert("You chose " + userGuess);
-    userGuessArray.push(userGuess);
-    console.log("The user has guessed " + userGuessArray)
-    // argument for matching variables
-    //will use index of to see if userguess is in array
-    if (randomLetter.indexOf(userGuess) >= 0) {
-    //loop through the length of the word/array to
-
-      for (let i = 0; i < randomWord.length; i++) {
-        if(userGuess === randomWord[i]){
-          console.log(randomWord[i])//works
-         blankArray[i] = userGuess;
-          //set blankArray to html
-        blankArrayText.textContent = blankArray.join(" ");
-         console.log("the blank is " + blankArray[i])
-        
-          console.log("The blank array is now " + blankArray);
-          scoreGame();
-        }
-        
-      }
-    }
-    else if(randomLetter.indexOf(userGuess) < 0){
-      console.log(randomWord + " does not have " + userGuess);
-      guesses--;
-      
-      console.log("You now have " + guesses + " guesses left");
-      scoreGame();
-    }
+    let userGuess = String.fromCharCode(event).toLowerCase();
+    gamePlay(userGuess);
+    scoreGame();
 
   
 };
   
-    //If blankArray === randomLetter, wins++;
-   
-
-    //if guesses = 0, losses++;
-
-    //
-//logic of comparing user input to hidden name
 
